cd6182e570e16cb5a24edfb5adff2526a0c6cdb8
tubbmeister <richard.hoskison@btinternet.com>
2017-09-19 17:12:59 +0100

    Fifth

--- app/src/main/java/com/example/richa_000/landingperformance/MainActivity.java
+++ app/src/main/java/com/example/richa_000/landingperformance/MainActivity.java
@@ -50,21 +50,54 @@ public class MainActivity extends AppCompatActivity  {
     public void sendMessage(View v) {
         EditText editText = (EditText) findViewById(R.id.editText);
         String weight = editText.getText().toString();
-        double myNum;
+        EditText elevnText = (EditText) findViewById(R.id.editText2);
+        String elevation = elevnText.getText().toString();
+        EditText tempText = (EditText) findViewById(R.id.editText3);
+        String temp = tempText.getText().toString();
+        EditText winddctnText = (EditText) findViewById(R.id.editText4);
+        String winddctn = winddctnText.getText().toString();
+        EditText windspeedtext = (EditText) findViewById(R.id.editText5);
+        String windspeed = windspeedtext.getText().toString();
+        EditText rwText = (EditText) findViewById(R.id.editText6);
+        String rwdctn = rwText.getText().toString();
+        EditText spdadjustText = (EditText) findViewById(R.id.editText7);
+        String spdadjust = spdadjustText.getText().toString();
+
+        double myNum,elevationNum,tempNum,winddctnNum,winspeedNum,rwdctnNum,spdadjustNum;
         try {
-            myNum = Double.parseDouble(weight);
+            myNum = Double.parseDouble(weight);//double because of decimal point
+            elevationNum=Double.parseDouble(elevation);
+            tempNum=Double.parseDouble(temp);
+            winddctnNum=Double.parseDouble(winddctn);
+            winspeedNum=Double.parseDouble(windspeed);
+            rwdctnNum=Double.parseDouble(rwdctn);
+            spdadjustNum=Double.parseDouble(spdadjust);
             //myNum=myNum+1000;
-            myNum=myNum-48000;
-            myNum=myNum/5000;
+           // myNum=myNum-48000;
+           // myNum=myNum/5000;
 
             weight=(""+myNum); //convert int to str
             Resources r = getResources();
             int[] bases = r.getIntArray(R.array.f30_dry_ab3);
 
-            double ref_dist;
+            double ref_dist,ref_dist1,ref_dist2,ref_dist3,ref_dist4,ref_dist5,ref_dist6,ref_dist7;
 
             ref_dist =  bases[0]; // get first element of array
             ref_dist=ref_dist+(myNum*bases[1]);
+            ref_dist1=(elevationNum/1000)*bases[3];
+            ref_dist2=(15-(elevationNum/1000)*2)-(tempNum);
+            if (ref_dist2>0) {
+                ref_dist2 = (ref_dist2 / 10) * bases[7];
+            }
+                else {
+                ref_dist2 = (ref_dist2 / 10) * bases[6];
+            }
+            ref_dist7=(spdadjustNum/10)*bases[8];
+
+
+
+
+
             weight=(""+ref_dist);
 
 
