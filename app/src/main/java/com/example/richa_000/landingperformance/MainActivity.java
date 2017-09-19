b8cc1011ac07e1d721746a87db7110c6399c2c33
tubbmeister <richard.hoskison@btinternet.com>
2017-09-19 12:17:50 +0100

    Third

--- app/src/main/java/com/example/richa_000/landingperformance/MainActivity.java
+++ app/src/main/java/com/example/richa_000/landingperformance/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.richa_000.landingperformance;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,15 +23,39 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText weightText = (EditText) findViewById(R.id.editText);
+        weightText.setOnFocusChangeListener(new OnFocusChangeListener() {
+
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    weightText.setText("", TextView.BufferType.EDITABLE);
+                }
+            }
 
 
+        });
+        final EditText elevationText = (EditText) findViewById(R.id.editText2);
+        elevationText.setOnFocusChangeListener(new OnFocusChangeListener() {
+
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    elevationText.setText("", TextView.BufferType.EDITABLE);
+                }
+            }
+
+
+        });
     }
-
-
     public void sendMessage(View v) {
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         TextView textView1 = (TextView) findViewById(R.id.textView);
         textView1.setText(message);
+        //================ Hide Virtual Key Board When  Clicking==================//
+
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+//======== Hide Virtual Keyboard =====================//
     }
 }
