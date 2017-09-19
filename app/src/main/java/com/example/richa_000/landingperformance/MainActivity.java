39a0e1fd2fd7d21264518b1df306f1343dcf5be1
tubbmeister <richard.hoskison@btinternet.com>
2017-09-19 09:11:48 +0100

    Second

--- app/src/main/java/com/example/richa_000/landingperformance/MainActivity.java
+++ app/src/main/java/com/example/richa_000/landingperformance/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.richa_000.landingperformance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.richa_000.landingperformance.R.id.textView;
+
+
+public class MainActivity extends AppCompatActivity  {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+    public void sendMessage(View v) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        TextView textView1 = (TextView) findViewById(R.id.textView);
+        textView1.setText(message);
     }
 }
