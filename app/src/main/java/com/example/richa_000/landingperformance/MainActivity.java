1ba89884f8c87238318c1186a09eab39e2b2b4fe
tubbmeister <richard.hoskison@btinternet.com>
2017-09-26 19:53:22 +0100

    15th

--- app/src/main/java/com/example/richa_000/landingperformance/MainActivity.java
+++ app/src/main/java/com/example/richa_000/landingperformance/MainActivity.java
@@ -144,27 +144,58 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
         String weight = editText.getText().toString();
         EditText elevnText = (EditText) findViewById(R.id.editText2);
+
         String elevation = elevnText.getText().toString();
-        EditText tempText = (EditText) findViewById(R.id.editText3);
-        String temp = tempText.getText().toString();
-        EditText winddctnText = (EditText) findViewById(R.id.editText4);
-        String winddctn = winddctnText.getText().toString();
-        EditText windspeedtext = (EditText) findViewById(R.id.editText5);
-        String windspeed = windspeedtext.getText().toString();
-        EditText rwText = (EditText) findViewById(R.id.editText6);
-        String rwdctn = rwText.getText().toString();
-        EditText spdadjustText = (EditText) findViewById(R.id.editText7);
-        String spdadjust = spdadjustText.getText().toString();
-
-        double myNum, elevationNum, tempNum, winddctnNum, winspeedNum, rwdctnNum, spdadjustNum;
-
-        String strUserName=editText.getText().toString();
-        if (strUserName.trim().equals("")) { //shows "Enter data" if nothing in first editText box
+        if (elevation.trim().equals("")) { //shows "Enter data" if nothing in first editText box
             Toast.makeText(getApplicationContext(), "Enter all data boxes filled!!",
 
                     Toast.LENGTH_SHORT).show();
             return; //quits method
         }
+        EditText tempText = (EditText) findViewById(R.id.editText3);
+        String temp = tempText.getText().toString();
+        if (temp.trim().equals("")) { //shows "Enter data" if nothing in first editText box
+            Toast.makeText(getApplicationContext(), "Enter all data boxes filled!!",
+
+                    Toast.LENGTH_SHORT).show();
+            return; //quits method
+        }
+        EditText winddctnText = (EditText) findViewById(R.id.editText4);
+        String winddctn = winddctnText.getText().toString();
+        if (winddctn.trim().equals("")) { //shows "Enter data" if nothing in first editText box
+            Toast.makeText(getApplicationContext(), "Enter all data boxes filled!!",
+
+                    Toast.LENGTH_SHORT).show();
+            return; //quits method
+        }
+        EditText windspeedtext = (EditText) findViewById(R.id.editText5);
+        String windspeed = windspeedtext.getText().toString();
+        if (windspeed.trim().equals("")) { //shows "Enter data" if nothing in first editText box
+            Toast.makeText(getApplicationContext(), "Enter all data boxes filled!!",
+
+                    Toast.LENGTH_SHORT).show();
+            return; //quits method
+        }
+        EditText rwText = (EditText) findViewById(R.id.editText6);
+        String rwdctn = rwText.getText().toString();
+        if (rwdctn.trim().equals("")) { //shows "Enter data" if nothing in first editText box
+            Toast.makeText(getApplicationContext(), "Enter all data boxes filled!!",
+
+                    Toast.LENGTH_SHORT).show();
+            return; //quits method
+        }
+        EditText spdadjustText = (EditText) findViewById(R.id.editText7);
+        String spdadjust = spdadjustText.getText().toString();
+        if (spdadjust.trim().equals("")) { //shows "Enter data" if nothing in first editText box
+            Toast.makeText(getApplicationContext(), "Enter all data boxes filled!!",
+
+                    Toast.LENGTH_SHORT).show();
+            return; //quits method
+        }
+
+        double myNum, elevationNum, tempNum, winddctnNum, winspeedNum, rwdctnNum, spdadjustNum;
+
+
 
 
 
